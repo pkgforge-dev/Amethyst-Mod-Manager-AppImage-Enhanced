fc2b45d2e7d34dcea25e6bfd1fa2ae927940e12a
Samueru-sama <xdglawyer@outlook.com>
2026-04-29 17:09:30 -0400

    package app

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,18 +3,26 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q amethyst-mod-manager | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/512x512/apps/io.github.Amethyst.ModManager.png
+export DESKTOP=/usr/share/applications/io.github.Amethyst.ModManager.desktop
+export DEPLOY_PYTHON=1
+export ALWAYS_SOFTWARE=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun \
+	/usr/bin/amethyst-mod-manager   \
+	/usr/share/amethyst-mod-manager \
+	/usr/bin/zenity                 \
+	/usr/lib/libgtk-3.so*           \
+	/usr/lib/libtcl8.6.so*          \
+	/usr/lib/libtk8.6.so
 
-# Additional changes can be done in between here
+sed -i -e 's|/usr/share|"$APPDIR"/share|g' ./AppDir/bin/amethyst-mod-manager
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
